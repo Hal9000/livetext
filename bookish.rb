4c6449d2f17ec38cb1d4cb87e6faa1429c01663f
Hal Fulton <rubyhacker@gmail.com>
2016-01-05 17:22:13 -0600

    Fixes and enhancements

---
+++ bookish.rb
@@ -0,0 +1,349 @@
+require 'pygments'
+
+###
+
+module PygmentFix   # Remove CSS for Jutoh
+  Styles = {
+    :c  => "#408080-i",  # Comment
+    :k  => "#008000-b",  # Keyword
+    :o  => "#666666",    # Operator
+    :cm => "#408080-i",  # Comment.Multiline
+    :cp => "#BC7A00",    # Comment.Preproc
+    :c1 => "#408080-i",  # Comment.Single
+    :cs => "#408080-i",  # Comment.Special
+    :kc => "#008000-b",  # Keyword.Constant
+    :kd => "#008000-b",  # Keyword.Declaration
+    :kn => "#008000-b",  # Keyword.Namespace
+    :kp => "#008000",    # Keyword.Pseudo
+    :kr => "#008000-b",  # Keyword.Reserved
+    :kt => "#B00040",    # Keyword.Type
+    :m  => "#666666",    # Literal.Number
+    :s  => "#BA2121",    # Literal.String
+    :na => "#7D9029",    # Name.Attribute
+    :nb => "#008000",    # Name.Builtin
+    :nc => "#0000FF-b",  # Name.Class
+    :no => "#880000",    # Name.Constant
+    :nd => "#AA22FF",    # Name.Decorator
+    :ni => "#999999-b",  # Name.Entity
+    :ne => "#D2413A-b",  # Name.Exception
+    :nf => "#0000FF",    # Name.Function
+    :nl => "#A0A000",    # Name.Label
+    :nn => "#0000FF-b",  # Name.Namespace
+    :nt => "#008000-b",  # Name.Tag
+    :nv => "#19177C",    # Name.Variable
+    :ow => "#AA22FF-b",  # Operator.Word
+    :w  => "#bbbbbb",    # Text.Whitespace
+    :mb => "#666666",    # Literal.Number.Bin
+    :mf => "#666666",    # Literal.Number.Float
+    :mh => "#666666",    # Literal.Number.Hex
+    :mi => "#666666",    # Literal.Number.Integer
+    :mo => "#666666",    # Literal.Number.Oct
+    :sb => "#BA2121",    # Literal.String.Backtick
+    :sc => "#BA2121",    # Literal.String.Char
+    :sd => "#BA2121-i",  # Literal.String.Doc
+    :s2 => "#BA2121",    # Literal.String.Double 
+    :se => "#BB6622-b",  #  Literal.String.Escape 
+    :sh => "#BA2121",    # Literal.String.Heredoc
+    :si => "#BB6688-b",  # Literal.String.Interpol
+    :sx => "#008000",    # Literal.String.Other
+    :sr => "#BB6688",    # Literal.String.Regex
+    :s1 => "#BA2121",    # Literal.String.Single
+    :ss => "#19177C",    # Literal.String.Symbol
+    :bp => "#008000",    # Name.Builtin.Pseudo
+    :vc => "#19177C",    # Name.Variable.Class
+    :vg => "#19177C",    # Name.Variable.Global
+    :vi => "#19177C",    # Name.Variable.Instance
+    :il => "#666666"     # Literal.Number.Integer.Long
+  }
+
+  def self.pyg_change(code, klass, style)
+    color = style[0..6]
+    modifier = style[8]
+    mod_open = modifier ? "<#{modifier}>" : ""
+    mod_close = modifier ? "</#{modifier}>" : ""
+    rx = /<span class="#{klass}">(?<cname>[^<]+?)<\/span>/
+    loop do
+      md = rx.match(code)
+      break if md.nil?
+      str = md[:cname]
+      result = code.sub!(rx, "<font color=#{color}>#{mod_open}#{str}#{mod_close}</font>")
+      break if result.nil?
+    end
+  end
+
+  def self.pyg_finalize2(code, lexer=:elixir)   # experimental
+    Styles.each_pair {|klass, style| pyg_change(code, klass, style) }
+    code.sub!(/<pre>/, "<pre>\n")
+    code.gsub!(/<span class="[np]">/, "")
+    code.gsub!(/<\/span>/, "")
+    color = case lexer
+      when :elixir
+        "#fc55fc"
+      when :ruby
+        "#fc5555"
+      else
+        raise "Unknown lexer"
+    end
+    code.gsub!(/ class="[^"]*?"/, "")    # Get rid of remaining Pygments CSS
+    lines = code.split("\n")
+    n1 = lines.index {|x| x =~ /<pre>/ }
+    n2 = lines.index {|x| x =~ /<\/pre>/ }
+    lines[n1].sub!(/ 1$/, "  1 ")
+    (n1+1).upto(n2) {|n| lines[n].replace(" " + lines[n] + " ") }
+    code = lines.join("\n")
+    code
+  end
+
+  def self._codebar_color(lexer)
+    color = case lexer
+      when :elixir
+        "#fc88fc"
+      when :ruby
+        "#fc8888"
+      else
+        raise "Unknown lexer"
+    end
+  end
+
+  def self.pyg_finalize(code, lexer=:elixir)
+    Styles.each_pair {|klass, style| pyg_change(code, klass, style) }
+    code.sub!(/<pre>/, "<pre>\n")
+    code.gsub!(/<span class="[np]">/, "")
+    code.gsub!(/<\/span>/, "")
+    color = _codebar_color(lexer)
+    code.sub!(/<td class="linenos"/, "<td width=5%></td><td width=5% bgcolor=#{color}")
+    code.gsub!(/ class="[^"]*?"/, "")    # Get rid of remaining Pygments CSS
+    lines = code.split("\n")
+    n1 = lines.index {|x| x =~ /<pre>/ }
+    n2 = lines.index {|x| x =~ /<\/pre>/ }
+    lines[n1].sub!(/ 1$/, "  1 ")
+    (n1+1).upto(n2) {|n| lines[n].replace(" " + lines[n] + " ") }
+    code = lines.join("\n")
+    code
+  end
+end
+
+
+###
+
+  def _process_code(text)
+    lines = text.split("\n")
+    lines = lines.select {|x| x !~ /##~ omit/ }
+    @refs = {}
+    lines.each.with_index do |line, i|
+      if line =~ /##~ ref/
+        frag, name = line.split(/ *##~ ref/)
+        @refs[name.strip] = i
+        line.replace(frag)
+      end
+    end
+    lines.map! {|line| "  " + line }
+    text.replace(lines.join("\n"))
+  end
+
+  def _colorize(code, lexer=:elixir)
+    text = ::Pygments.highlight(code, lexer: lexer, options: {linenos: "table"})
+    PygmentFix.pyg_finalize(text, lexer)
+  end
+
+  def _colorize2(code, lexer=:elixir)  # experimental
+    text = ::Pygments.highlight(code, lexer: lexer, options: {})
+    PygmentFix.pyg_finalize(text, lexer)
+  end
+
+  def ruby2   # experimental
+    file = @_args.first 
+    if file.nil?
+      code = ""
+      _body {|line| code << line }
+    else
+      code = ::File.read(file)
+    end
+
+    _process_code(code)
+    html = _colorize2(code, :ruby)
+    ### New code...
+    lines = html.split("\n")[1..-2]
+    code = "<table cellspacing=0>"
+    lines.each.with_index do |line,i|
+      line.chomp!
+      code << "<tr><td width=5% align=right bgcolor=#fc5555><pre>#{i}</pre></td>\n"
+      code << "    <td><pre>#{line}</pre></td>\n"
+      code << "</tr>"
+    end
+    code << "</table>"
+    ### ...end
+    @output.puts "\n#{code}\n "
+  end
+
+  def ruby
+    file = @_args.first 
+    if file.nil?
+      code = ""
+      _body {|line| code << line }
+    else
+      code = ::File.read(file)
+    end
+
+    _process_code(code)
+    html = _colorize(code, :ruby)
+    @output.puts "\n#{html}\n "
+  end
+
+  def elixir
+    file = @_args.first 
+    if file.nil?
+      code = ""
+      _body {|line| code << line }
+    else
+      code = ::File.read(file)
+    end
+
+    _process_code(code)
+    html = _colorize(code, :elixir)
+    @output.puts "\n#{html}\n "
+  end
+
+  def fragment
+    lexer = @_args.empty? ? :elixir : @_args.first.to_sym   # ruby or elixir
+    code = ""
+    code << "# Ruby code\n\n" if lexer == :ruby
+    _body {|line| code << line }
+    text = ::Pygments.highlight(code, lexer: lexer, options: {})  # no line numbers
+    PygmentFix.pyg_finalize(text, lexer)
+    @output.puts text + "\n<br>"
+  end
+
+  def code
+    text = ""
+    _body {|line| @output.puts "    " + line }
+  end
+
+include PygmentFix
+
+def hardbreaks
+  @hard = false
+  @hard = true unless @_args.first == "off"
+end
+
+def hardbreaks?
+  @hard
+end
+
+def list
+  @output.puts "<ul>"
+  _body {|line| @output.puts "<li>#{line}</li>" }
+  @output.puts "</ul>"
+end
+
+def alpha_columns
+  n = @_args.first.to_i   # FIXME: what if missing?
+  words = []
+  _body {|line| words << line }
+  words.sort!
+  @output.puts "<table>"
+  words.each_slice(n) do |*w|
+    @output.puts "<tr><td>" + w.join("</td><td>") + "</td></tr>"
+  end
+  @output.puts "</table>"
+end
+
+def comment
+  _body {|line| }  # ignore body
+end
+
+def _errout(*args)
+  ::STDERR.puts *args
+end
+
+def chapter
+# _errout("chapter")
+  @chapter = @_args.first.to_i
+  @sec = @sec2 = 0
+  title = @_data.split(" ",2)[1]
+  @toc << "#@chapter #{title}"
+  @output.puts "<title>#{@chapter}. #{title}</title>"
+  @output.puts <<-HTML
+    <h2>Chapter #{@chapter}</h1>
+    <h1>#{title}</h1>
+
+  HTML
+end
+
+def sec
+  @sec += 1
+  @sec2 = 1
+  @section = "#@chapter.#@sec"
+# _errout("section #@section")
+  @toc << "   #@section #@_data"
+  @output.puts "<br><h3>#@section #{@_data}</h3>\n"
+end
+
+def subsec
+  @sec2 += 1
+  @subsec = "#@chapter.#@sec.#@sec2"
+  @toc << "      #@subsec #@_data"
+# _errout("section #@subsec")
+  @output.puts "<br><h3>#@subsec #{@_data}</h3>\n"
+end
+
+def table
+  num, title = @_data.split(" ", 2)
+  delim = "~~"
+  @output.puts "<br><center><table border=1 width=90% cellpadding=5>"
+  _body do |line|
+    cells = line.split(delim)
+    @output.puts "<tr>"
+    cells.each {|cell| @output.puts "  <td>#{cell}</td>" }
+    @output.puts "</tr>"
+  end
+  @output.puts "</table>"
+  @toc << "        Table #{num} #{title}"
+  @output.puts "<br><b>Table #{num} &nbsp;&nbsp; #{title}</b></center><br>"
+end
+
+def toc
+  file = @_args.first
+  if file.nil?
+    @toc = []
+  else
+    ::File.write(file, @toc.join)
+  end
+end
+
+def table_old
+  rows, cols, num, title = @_data.split(" ", 4)
+  rows = rows.to_i
+  cols = cols.to_i
+  delim = "~~"
+  @output.puts "<br><center><table border=1 width=90% cellpadding=5>"
+  rows.times do |row|
+    cells = @input.next.split(delim)
+    nc = cells.size
+    raise "Expected #{cols} cells, not #{nc}" if nc != cols
+    @output.puts "<tr>"
+    cells.each {|cell| @output.puts "  <td>#{cell}</td>" }
+    @output.puts "</tr>"
+  end
+  @output.puts "</table>"
+  @output.puts "<br><b>Table #{num} &nbsp;&nbsp; #{title}</b></center><br>"
+end
+
+def missing
+  @output.puts "<br><font color=red><i>[Material missing"
+  @output.puts @_data unless @_data.empty?
+  @output.puts "]</i></font><br>\n "
+end
+
+def note
+  @output.puts "<br><font color=red><i>Note: "
+  @output.puts @_data 
+  @output.puts "</i></font><br>\n "
+end
+
+def quote
+  @output.puts "<blockquote>"
+  @output.puts _body
+  @output.puts "</blockquote>"
+end
+
--- livetext.rb
+++ livetext.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
+class Enumerator
+  def remaining
+    array = []
+    loop { array << self.next }
+    array
+  end
+end
+
 class Livetext
   Version = "0.0.1"
 
@@ -65,29 +75,29 @@ module Livetext::Helpers
   def _raw_body(tag = "__EOF__", sigil = Livetext::MainSigil)
     lines = []
     loop do
-      line = @input.next  # no chomp needed
-      break if line.chomp.strip == tag
-      lines << line
+      @line = _next_line
+      break if @line.chomp.strip == tag
+      lines << @line
     end
     if block_given?
-      lines.each {|line| yield line }
+      lines.each {|line| yield @line }
     else
-      lines.each.to_a
+      lines
     end
   end
 
   def _body(sigil=Livetext::MainSigil)
     lines = []
     loop do
-      line = @input.next  # no chomp needed
-      break if _end?(line, sigil)
-      next if _comment?(line, sigil)
-      lines << line  # _formatting(line)  # FIXME ?? 
+      @line = _next_line  # no chomp needed
+      break if _end?(@line, sigil)
+      next if _comment?(@line, sigil)
+      lines << @line  # _formatting(line)  # FIXME ?? 
     end
     if block_given?
       lines.each {|line| yield line }
     else
-      lines.each.to_a
+      lines
     end
   end
 
@@ -133,6 +143,13 @@ module Livetext::Helpers
   def _print(*args)
     @output.print *args
   end
+
+  def _next_line
+    @line = @input.next
+    @lnum ||= 0
+    @lnum += 1
+    @line
+  end
 end
 
 module Livetext::Standard
@@ -161,26 +178,27 @@ module Livetext::Standard
   end
 
   def set
-    assigns = _data.split(",")
+    assigns = _data.chomp.split(",")
     assigns.each do |a| 
       var, val = a.split("=")
       @vars[var] = val
     end
   end
 
-  def include
+  def _include
     file = _args.first
     lines = ::File.readlines(file)
-    array = lines + @input.to_a
+    rem = @input.remaining
+    array = lines + rem
     @input = array.each # FIXME .with_index
   end
 
   def mixin
-    _args.each do |arg|
-      file = arg + ".rb"
-      text = ::File.read(file)
-      self.class.class_eval(text)
-    end
+    file = _args.first + ".rb"
+    return if @_mixins.include?(file)
+    @_mixins << file
+    text = ::File.read(file)
+    self.class.class_eval(text)
   end
 
   def copy
@@ -208,6 +226,7 @@ class Livetext::System < BasicObject
     @output = output
     @tty = ::File.open("/dev/tty", "w")
     @vars = {}
+    @_mixins = []
   end
 
   def method_missing(name, *args)
@@ -230,24 +249,25 @@ end
 def handle_sscomment(sigil, line)
 end
 
+def _get_name(obj, sigil, line)
+  blank = line.index(" ") || line.index("\n")
+  name = line[1..(blank-1)]
+  abort "Name '#{name}' is not permitted" if Livetext::Disallowed.include?(name.to_sym)
+  obj._data = line[(blank+1)..-1]
+  name = "_def" if name == "def"
+  name = "_include" if name == "include"
+  name
+end
+
 def handle_ssname(sigil, line)
   obj = Livetext::Objects[sigil]
-  blank = line.index(" ")
-  name = line[2..(blank-1)]
-  abort "Name '#{name}' is not permitted" if Livetext::Disallowed.include?(name.to_sym)
-  obj._data = line[(blank+1)..-1].chomp
-  name = "_def" if name == "def"
+  name = _get_name(obj, sigil, line)
   obj.send(name)
 end
 
 def handle_sname(sigil, line)
   obj = Livetext::Objects[sigil]
-  line = line.chomp
-  blank = line.index(" ") || line.size  # maybe no blank?
-  name = line[1..(blank-1)]
-  abort "Name '#{name}' is not permitted" if Livetext::Disallowed.include?(name.to_sym)
-  obj._data = line[(blank+1)..-1]
-  name = "_def" if name == "def"
+  name = _get_name(obj, sigil, line)
   obj.send(name)
 end
 
@@ -276,13 +296,13 @@ end
 
 
 if $0 == __FILE__
-  file = File.open(ARGV[0])
+  file = File.open(ARGV[0]) rescue STDIN
 
   source = file.each_line
   sys = Livetext::Objects[Livetext::MainSigil] = Livetext::System.new(source)
 
   loop do
-    line = sys._source.next
+    line = sys._next_line
     handle(line)
   end
 end
