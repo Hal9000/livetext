ed3f8e960920b5195020fc5a93117788acf7bef4
Hal Fulton <rubyhacker@gmail.com>
2021-12-21 21:42:05 -0600

    adds Snapshot class

--- test/snapshots.rb
+++ test/snapshots.rb
@@ -26,6 +26,88 @@ The external_files method works this way:
 
 class TestingLivetext < MiniTest::Test
 
+  class Snapshot
+    SOURCE = "source.lt3"
+    # Will now keep "actual" output in same dir?
+    ACTUAL_OUT, ACTUAL_ERR = "actual-output.txt", "actual-error.txt"
+    EXP_OUT,    EXP_ERR    = "expected-output.txt", "expected-error.txt"
+    MATCH_OUT,  MATCH_ERR  =  "match-output.txt", "match-error.txt"
+
+    def initialize(base)
+      @base = base
+      @errors = false
+      Dir.chdir(base) do
+        @literal_out = File.exist?(EXP_OUT)
+        @literal_err = File.exist?(EXP_ERR)
+        @match_out = File.exist?(MATCH_OUT)
+        @match_err = File.exist?(MATCH_ERR)
+      end
+      bad_files = (@literal_out && @match_out) || (@literal_err && @match_err)
+      raise "Inconsistent structure for #@base" if bad_files
+    end
+
+    def check_matches(actual, control)
+      controls = File.readlines(control).map(&:chomp)
+      lines = File.readlines(actual).map(&:chomp)
+      lines.unshift("DUMMY LINE")   # 1-based index! (for when I'm editing)
+      controls.each do |line|
+        line_num, item = line.split(" ", 2)
+        item = Regexp.new(item[1..-2]) if item[0] == "/"  # Hmm, string can't start with /...
+        line_num = line_num.to_i
+        info = "Expected line #{line_num} of #{actual.inspect} to match #{item.inspect} (was: #{lines[line_num].inspect})"
+        good = item === lines[line_num]
+        @errors = true unless good
+        assert item === lines[line_num], info   # string or regex
+      end
+    end
+
+    def sdiff(f1, f2, out)
+      File.open(out, "w") {|file| file.puts "#{'%-60s'% 'EXPECTED'}| #{'%-60s'% 'ACTUAL'}" }
+      system("/usr/bin/sdiff -t -w 121 #{f1} #{f2} >>#{out}")
+    end
+
+    def check_stdout
+      if @literal_out
+        actual, expected = File.read(ACTUAL_OUT), File.read(EXP_OUT)
+        same = assert actual == expected
+        @errors = true if not same
+        file = "out-sdiff.txt"
+        sdiff(ACTUAL_OUT, EXP_OUT, file)
+        assert same, "Discrepancy in STDOUT - see #@base/#{file}"
+      else
+        check_matches(ACTUAL_OUT, MATCH_OUT)
+      end
+    end
+
+    def check_stderr
+      if @literal_err
+        actual, expected = File.read(ACTUAL_ERR), File.read(EXP_ERR)
+        same = assert actual == expected
+        @errors = true if not same
+        file = "err-sdiff.txt"
+        sdiff(ACTUAL_ERR, EXP_ERR, file)
+        assert same, "Discrepancy in STDERR - see #@base/#{file}"
+      else
+        check_matches(ACTUAL_ERR, MATCH_ERR)
+      end
+    end
+
+    def cleanup
+      return if @errors
+      system("rm -f #{ACTUAL_OUT} #{ACTUAL_ERR} *sdiff.txt")
+    end
+
+    def run
+      Dir.chdir(@base) do
+        cmd = "bin/livetext #{SOURCE} >#{ACTUAL_OUT} 2>#{ACTUAL_ERR}"
+        system(cmd)
+        check_stdout
+        check_stderr
+        cleanup
+      end
+    end
+  end
+
   TTY = File.open("/dev/tty","w")
 
   cmdline = ARGV.first == "cmdline"
@@ -70,9 +152,18 @@ class TestingLivetext < MiniTest::Test
   selected = File.readlines("subset.txt").map(&:chomp)
   Subset   = selected.empty? ? TestDirs : selected
 
+# Old code:
+
+# Subset.each do |tdir|
+#   define_method("test_#{tdir}") do
+#     external_files(tdir)
+#   end
+# end
+
   Subset.each do |tdir|
     define_method("test_#{tdir}") do
-      external_files(tdir)
+      this = Snapshot.new(tdir)
+      this.run
     end
   end
 
@@ -90,15 +181,6 @@ class TestingLivetext < MiniTest::Test
     system("/usr/bin/sdiff -t -w 121 #{f1} #{f2} >>#{out}")
     return "\n  >>> Unexpected std#{which}! See #{out}"
   end
-
-  def NEW_external_files(base)
-      src, out, exp = "source.lt3", "/tmp/#{base}--actual-output.txt", "expected-output.txt"
-      err, erx = "/tmp/#{base}--actual-error.txt", "expected-error.txt"
-      out_match = "out-match.txt"
-      err_match = "err-match.txt"
-
-  end 
-
 
   def external_files(base)
     Dir.chdir(base) do
