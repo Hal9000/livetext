7977dd8a402433d06549252b147085ba7b9baece
Mykl Clason <clason.rails.dev@gmail.com>
2022-01-14 05:01:45 +0000

    Get bookish snapshot test passing

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -55,7 +55,7 @@ module Helpers
     @backtrace = btrace
     @main.source(enum, fname, 0)
     line = nil
-    loop do 
+    loop do
       line = @main.nextline
       break if line.nil?
       process_line(line)
@@ -95,21 +95,21 @@ module Helpers
         result = @main.send(name)
 
         # NOTE: The above line is where the magic happens!
-        # A name like 'foobar' results in an invocation of 
+        # A name like 'foobar' results in an invocation of
         # @main.foobar (where @main is a Processor, and any
         # new methods (e.g. from a mixin) are added to @main
-        # 
+        #
         # So all the functionality from _args and _raw_args
         # and _data (among others?) will be encapsulated in
-        # 'some' kind of PORO which handles access to all 
+        # 'some' kind of PORO which handles access to all
         # these things as well as the 'body' between the
         # command and its corresponding .end
-        # 
+        #
         # The 'body' functionality is so commonly used, I plan
         # to pass it in separately as needed (even though the
         # args object should make it available also).
-        # 
-        # Every method corresponding to a dot commmand will 
+        #
+        # Every method corresponding to a dot commmand will
         # get args and body passed in as needed. Every one of
         # the signatures already has (args = nil, body = nil)
         # but nothing is being passed in that way yet.
